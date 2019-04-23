f8048a3592c2f77922c80f8f8a690fa7372af4d5
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-04-23 20:56:51 +0300

    Added use cases

---
+++ BLL.Interface/Models/Room.cs
@@ -0,0 +1,14 @@
+﻿namespace BLL.Interface.Models
+{
+	public class Room
+	{
+		public int RoomId { get; set; }
+
+		public int LivingRoomsCount { get; set; }
+
+		public int BedsCount { get; set; }
+
+		public string Description { get; set; }
+
+	}
+}
