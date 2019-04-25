21ccd6e87130efd380d5eb6a9336177c8f8261ba
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-04-25 23:56:44 +0300

    Added BLL

---
+++ BLL/Services/HotelService.cs
@@ -0,0 +1,33 @@
+﻿using BLL.Interface.Interfaces;
+using BLL.Interface.Models;
+using DAL.Interfaces;
+
+namespace BLL.Services
+{
+	/// <summary>
+	/// hotel service
+	/// </summary>
+	public class HotelService : IHotelService
+	{
+		private readonly IHotelRepository hotelRepository;
+
+		public HotelService(IHotelRepository hotelRepository)
+		{
+			this.hotelRepository = hotelRepository;
+		}
+
+		public Hotel GetHotel()
+		{
+			var hotelDB = hotelRepository.GetHotel();
+
+			return new Hotel
+			{
+				Id = hotelDB.Id,
+				Description = hotelDB.Description,
+				Name = hotelDB.Name,
+				PhoneNumber = hotelDB.PhoneNumber,
+				Address = hotelDB.Address
+			};
+		}
+	}
+}
