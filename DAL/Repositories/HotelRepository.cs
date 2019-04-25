5ccca7feec34e8a03a04da0272dbcee4695d0eaf
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-04-25 23:49:10 +0300

    Added template for site

---
+++ DAL/Repositories/HotelRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+	public class HotelRepository : IHotelRepository
+	{
+		public Hotel GetHotel()
+		{
+			using(DataModelContainer container = new DataModelContainer())
+			{
+				var hotel = container.Hotels.FirstOrDefault();
+
+				return hotel;
+			}
+		}
+	}
+}
---
+++ WebUI/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using BLL.Interface.Interfaces;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AboutController : Controller
+    {
+		private readonly IHotelService service;
+
+		public AboutController(IHotelService service)
+		{
+			this.service = service;
+		}
+
+        public ActionResult Index()
+        {
+			var model = service.GetHotel();
+
+			return View(model);
+        }
+	}
+}
