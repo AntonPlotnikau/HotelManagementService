2b007420716f91baa5e1a96ad0c24d7d04aef97a
mtsyvis <mtsyvis2405@gmail.com>
2019-04-28 20:29:09 +0300

    Add booking control view

--- WebUI/Controllers/RoomsController.cs
+++ WebUI/Controllers/RoomsController.cs
@@ -14,11 +14,68 @@ namespace WebUI.Controllers
             this.roomService = roomService;
         }
 
-        public ActionResult OneColumn()
+        public ActionResult RoomsBooking()
         {
             var model = this.roomService.GetRooms();
             return View(model.ToList());
         }
+
+        public ActionResult ControlRoomService()
+        {
+            var model = this.roomService.GetRooms();
+            return View(model.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult DeleteRoom(string id)
+        {
+            var model = this.roomService.GetRoom(int.Parse(id));
+            return this.View(model);
+        }
+
+        //[HttpPost]
+        //public ActionResult Delete(string isbn, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        this.service.Remove(isbn);
+
+        //        return this.RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        ViewBag.Error = "This book is not kept!";
+        //        return this.View();
+        //    }
+        //}
+        //public ActionResult Details(string isbn)
+        //{
+        //    var findBook = this.service.FindByTag(isbn);
+
+        //    return this.View(findBook);
+        //}
+
+        //[HttpGet]
+        //public ActionResult Create()
+        //{
+        //    return this.View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Create(Book book)
+        //{
+        //    try
+        //    {
+        //        this.service.Add(book);
+
+        //        return this.RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        ViewBag.Error = "This book already saved!";
+        //        return this.View();
+        //    }
+        //}
 
         public ActionResult SingleItem()
         {
