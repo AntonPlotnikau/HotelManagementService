c22456addc71cae8eccab26b3bb836b52368a8e6
AntonPlotnikau <anton.plotnikau@gmail.com>
2019-05-02 21:43:52 +0300

    Added interface

---
+++ BLL.Interface/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using BLL.Interface.Models;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BLL.Interface.Interfaces
+{
+	public interface IUserService
+	{
+		Task RegisterUserAsync(User user);
+
+		Task<User> FindByLoginAsync(string userName);
+
+		Task<ClaimsIdentity> CreateClaimAsync(string userName, string password);
+	}
+}
